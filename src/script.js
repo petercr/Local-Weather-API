f7b262a881f88ba75b59d0a1ac4b8e3af5fda1ab
Peter <petecruckshank@gmail.com>
2017-08-31 21:46:31 -0400

    put js into /src/script.js for linting

---
+++ src/script.js
@@ -0,0 +1,29 @@
+
+function geoFindMe() {
+var output = document.getElementById("location");
+
+if (!navigator.geolocation){
+output.innerHTML = "Geolocation is not supported by your browser";
+return;
+}
+
+function success(position) {
+var latitude  = position.coords.latitude;
+var longitude = position.coords.longitude;
+
+output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+var img = new Image();
+img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+output.appendChild(img);
+}
+
+function error() {
+output.innerHTML = "Unable to retrieve your location";
+}
+
+output.innerHTML = "<p>Locating…</p>";
+
+navigator.geolocation.getCurrentPosition(success, error);
+}
