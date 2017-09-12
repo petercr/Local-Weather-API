3fdb85ce7bfc2cc032d7cbfc171c2a01e57dbdb1
Peter <petecruckshank@gmail.com>
2017-09-12 19:30:57 -0400

    Changed syntax to match ESLint rules
    ESLint spacing formatting applied as well as double quote for strings

--- src/script.js
+++ src/script.js
@@ -2,41 +2,41 @@ var latitude, longitude;  // Longitude and Latitude variables
 var Ftemp, Ctemp; // Fahrenheit and Celcius variables
 
 function geoFindMe() {
-    var output = document.getElementById("location");
+	var output = document.getElementById("location");
 
-    if (!navigator.geolocation) {
-        output.innerHTML = "Geolocation is not supported by your browser";
-        return;
-    }
+	if (!navigator.geolocation) {
+		output.innerHTML = "Geolocation is not supported by your browser";
+		return;
+	}
 
-    function success(position) {
-        latitude = position.coords.latitude.toFixed(2);
-        longitude = position.coords.longitude.toFixed(2);
+	function success(position) {
+		latitude = position.coords.latitude.toFixed(2);
+		longitude = position.coords.longitude.toFixed(2);
 
-        output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+		output.innerHTML = "<p>Latitude is " + latitude + "° <br>Longitude is " + longitude + "°</p>";
 
-        var api = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon="+ longitude + "&units=imperial" + "&APPID=9bc2e8db193857d51a6764aae51aa49c";
+		var api = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon="+ longitude + "&units=imperial" + "&APPID=9bc2e8db193857d51a6764aae51aa49c";
 
-        // jQuery AJAX call FreeCodeCamp Weather API
-        $.getJSON(api, function(data) {
-            Ftemp = data.main.temp;
-            Ctemp = (Ftemp - 32) * (5 / 9);
-            var city = data.name;
-            $('#temp').html(data.main.temp + " &deg; F");
-            $('#city').html(Ctemp.toFixed(2) + "&deg; C");
-            $('#condition').html("You are " + city + " " + data.sys.country);
-            console.log(api);
-        });
+		// jQuery AJAX call FreeCodeCamp Weather API
+		$.getJSON(api, function(data) {
+			Ftemp = data.main.temp;
+			Ctemp = (Ftemp - 32) * (5 / 9);
+			var city = data.name;
+			$("#temp").html(data.main.temp + " &deg; F");
+			$("#city").html(Ctemp.toFixed(2) + "&deg; C");
+			$("#condition").html("You are " + city + " " + data.sys.country);
+			console.log(api);
+		});
 
-    } // end success
+	} // end success
 
-    function error() {
-        output.innerHTML = "Unable to retrieve your location";
-    }
+	function error() {
+		output.innerHTML = "Unable to retrieve your location";
+	}
 
-    output.innerHTML = "<p>Locating…</p>";
+	output.innerHTML = "<p>Locating…</p>";
 
-    navigator.geolocation.getCurrentPosition(success, error);
+	navigator.geolocation.getCurrentPosition(success, error);
 
 
 
