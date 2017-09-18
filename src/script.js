3ade253001b8cf6fb68b04561fe56f0174fbb53c
Peter <petecruckshank@gmail.com>
2017-09-17 20:10:32 -0400

    adding in weathericons.io in place of icon link
    used different SVG weather icons for different conditions

--- src/script.js
+++ src/script.js
@@ -25,59 +25,74 @@ function geoFindMe() {
 			var city = data.name;
 			var weather = data.weather[0].description;
 			var icon = data.weather[0].icon;
-			console.log("icon number is " + icon);
 			var iconURL = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
 
 			$("#temp").html(Math.round(data.main.temp) + " &deg; F"); // displays degrees Far
 			$("#location").html("You are " + city + " " + ", " + data.sys.country); // displays city and country
 			$("#condition").html(weather); // displays the weather condition
-			$("#icons").html("<img id='picture' src=" + iconURL + ">"); // adds an img tag with icon from API
+			
 
 			switch (icon) { // switch statement to change background image for each weather type
-			case "01d":
+			case "01d": //clear skys daytime
 				$(".container").css("background-image", "url('/img/clearDay.jpg')");
+				$("#icons").html("<i class='wi wi-day-sunny'></i>");
 				break;
-			case "01n":
+			case "01n": // clear sky night
 				$(".container").css("background-image", "url('/img/clearSkyNight.jpg')");
+				$("#icons").html("<i class='wi wi-night-clear'></i>");
 				break;
-			case "02d":
+			case "02d": // few clouds day
 				$(".container").css("background-image", "url('/img/fewCloudDay.jpg')");
+				$("#icons").html("<i class='wi wi-day-cloudy'></i>");
 				break;
-			case "02n":
+			case "02n": // few clouds night
 				$(".container").css("background-image", "url('/img/fewCloudsNight.jpg')");
+				$("#icons").html("<i class='wi wi-night-cloudy'></i>");
 				break;
-			case "03d":
+			case "03d": // scattered clouds day
 				$(".container").css("background-image", "url('/img/scatteredCloudsDay.jpg')");
+				$("#icons").html("<i class='wi wi-day-sunny-overcast'></i>");
 				break;
-			case "03n":
+			case "03n": // scattered clouds night
 				$(".container").css("background-image", "url('/img/scatteredCloudNight.jpg')");
+				$("#icons").html("<i class='wi wi-night-partly-cloudy'></i>");
 				break;
-			case "04d":
+			case "04d": // broken clouds day
 				$(".container").css("background-image", "url('/img/scatteredCloudsDay.jpg')");
+				$("#icons").html("<i class='wi wi-day-cloudy-high'></i>");
 				break;
-			case "04n":
+			case "04n": // broken clouds night
 				$(".container").css("background-image", "url('/img/scatteredCloudsNight.jpg')");
+				$("#icons").html("<i class='wi wi-night-partly-cloudy'></i>");
 				break;
 			
-			case "09d":
-			case "10d":
+			case "09d": // showers and rain day
+			case "10d": // rain day
+			case "11d": // thunderstomrs
 				$(".container").css("background-image", "url('/img/dayRain.jpg')");
+				$("#icons").html("<i class='wi wi-day-rain'></i>");
 				break;
 			
-			case "10n":
-			case "11d":
+			case "09n": // showers and rain night
+			case "10n": // rain night
+			case "11n": // thunderstorm night
 				$(".container").css("background-image", "url('/img/nightRain.jpg')");
+				$("#icons").html("<i class='wi wi-night-rain'></i>");
 				break;
 			
-			case "50d":
+			case "50d": // mist day
 				$(".container").css("background-image", "url('/img/mistDay')");
+				$("#icons").html("<i class='wi wi-day-fog'></i>");
 				break;
-			case "50n":
+			case "50n": // mist night
 				$(".container").css("background-image", "url('/img/mistDay.jpg')");
+				$("#icons").html("<i class='wi wi-night-fog'></i>");
 				break;
 
 			default:
 				$(".container").css("background-image" , "url('/img/nightRain.jpg')");
+				$("#icons").html("<i class='wi wi-day-sunny'></i>");
+				console.log("something went wrong");
 			}
 
 		});
